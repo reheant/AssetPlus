4372d9324456af2776539cccef1425ddeea0366a
Tiffany <tiffanymiller0708@gmail.com>
2023-10-10 01:05:39 -0400

    Implemented controller for asset type

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
@@ -1,20 +1,61 @@
 package ca.mcgill.ecse.assetplus.controller;
+import ca.mcgill.ecse.assetplus.model.*;
 
 public class AssetPlusFeatureSet2Controller {
+    private static AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
+    public static String addAssetType(String name, int expectedLifeSpanInDays) {
+        //input validation
+        var error = "";
+        if (name == null) {
+            error = "A valid name must be inputted to add asset type";
+        }
+        if (expectedLifeSpanInDays <= 0) {
+            error += "Not a valid life span input"
+        }
+        if (!error.isEmpty()) {
+            return error.trim();
+        }
 
-  public static String addAssetType(String name, int expectedLifeSpanInDays) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
-  }
+        //call model
+        try {
+            assetPlus.addAssetType(name, expectedLifeSpanInDays);
+        } catch (RuntimeException e) {
+            return e.getMessage();
+        }
+        return "";
+    }
 
   public static String updateAssetType(String oldName, String newName, int newExpectedLifeSpanInDays) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+        //input validation
+        var error = "";
+        AssetType assetType = AssetType.getWithName(oldName)
+        if (assetType == null) {
+            error = "A valid old asset type name must be inputted";
+        }
+        if (expectedLifeSpanInDays <= 0){
+            error+= "Not a valid life span input"
+        }
+        if (!error.isEmpty()) {
+            return error.trim();
+        }
+
+        //call model
+        try {
+            assetType.setName(newName);
+            assetType.setExpectedLifeSpan(newExpectedLifeSpanInDays);
+        } catch (RuntimeException e) {
+            return e.getMessage();
+        }
+        return "";
   }
 
   public static void deleteAssetType(String name) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+      //call model
+      AssetType assetType = AssetType.getWithName(name)
+      if (assetType != null) {
+          assetType.delete();
+      }
+
   }
 
 }

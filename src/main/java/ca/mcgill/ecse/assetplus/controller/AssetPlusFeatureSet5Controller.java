9cefdb4d72123203b1ece7e78f4aa135a8f12399
Luke <luke.freund@mail.mcgill.ca>
2023-10-09 22:27:36 -0400

    Initial implementation of controller

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -1,15 +1,38 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import ca.mcgill.ecse.assetplus.model.MaintenanceTicket;
+import ca.mcgill.ecse.assetplus.model.TicketImage;
+
+import java.util.Objects;
+
 public class AssetPlusFeatureSet5Controller {
 
   public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+
+    MaintenanceTicket ticket;
+    try {
+      ticket = MaintenanceTicket.getWithId(ticketID);
+    } catch (Exception e) {
+      return "Ticket not found";
+    }
+    TicketImage image = new TicketImage(imageURL, ticket);
+    return String.format("Added image %s to ticket %d", image, ticketID);
   }
 
   public static void deleteImageFromMaintenanceTicket(String imageURL, int ticketID) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    MaintenanceTicket ticket;
+    try {
+      ticket = MaintenanceTicket.getWithId(ticketID);
+    } catch (Exception e) {
+      // Log error
+      return;
+    }
+    for (TicketImage image: ticket.getTicketImages()){
+      if (image.getImageURL().equals(imageURL)){
+        image.delete();
+        return;
+      }
+    }
   }
 
 }

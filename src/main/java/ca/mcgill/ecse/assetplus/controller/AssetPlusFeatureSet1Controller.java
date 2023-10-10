fb6056775fadcbeb510dc4958bae0c8674914b65
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2023-10-10 08:29:19 -0400

    try-except blocks and AP init

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet1Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet1Controller.java
@@ -1,54 +1,65 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import ca.mcgill.ecse.assetplus.model.*;
 
 public class AssetPlusFeatureSet1Controller {
+    private static AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
 
-  public static String updateManager(String password) {
-    AssetPlus assetPlus = new AssetPlus();  
-    Manager manager = assetPlus.getManager();
+    public static String updateManager(String password) {
+        try {
+            Manager manager = assetPlus.getManager();
 
-    if (manager != null) {
-      manager.setPassword(password);  
-      return "Manager password updated successfully.";
-    } else {
-      return "Manager not found.";
-    }
-  }
-
-  public static String addEmployeeOrGuest(String email, String password, String name, String phoneNumber, boolean isEmployee) {
-    AssetPlus assetPlus = new AssetPlus(); 
-
-    if (isEmployee) {
-      assetPlus.addEmployee(email, name, password, phoneNumber);
-      return "Employee added successfully.";
-    } else {
-      assetPlus.addGuest(email, name, password, phoneNumber);
-      return "Guest added successfully.";
-    }
-  }
-
-  public static String updateEmployeeOrGuest(String email, String newPassword, String newName, String newPhoneNumber) {
-    AssetPlus assetPlus = new AssetPlus();  
-
-    for (Employee employee : assetPlus.getEmployees()) {
-      if (employee.getEmail().equals(email)) {  
-        employee.setPassword(newPassword);  
-        employee.setName(newName);  
-        employee.setPhoneNumber(newPhoneNumber);
-        return "Employee updated successfully.";
-      }
+            if (manager != null) {
+                manager.setPassword(password);
+                return "Manager password updated successfully.";
+            } else {
+                return "Manager not found.";
+            }
+        } catch (NullPointerException e) {
+            return "Error: AssetPlus or Manager is not initialized.";
+        } catch (Exception e) {
+            return "An unexpected error occurred: " + e.getMessage();
+        }
     }
 
-    for (Guest guest : assetPlus.getGuests()) {
-      if (guest.getEmail().equals(email)) {  
-        guest.setPassword(newPassword); 
-        guest.setName(newName); 
-        guest.setPhoneNumber(newPhoneNumber);  
-        return "Guest updated successfully.";
-      }
+    public static String addEmployeeOrGuest(String email, String password, String name, String phoneNumber, boolean isEmployee) {
+        try {
+            if (isEmployee) {
+                assetPlus.addEmployee(email, name, password, phoneNumber);
+                return "Employee added successfully.";
+            } else {
+                assetPlus.addGuest(email, name, password, phoneNumber);
+                return "Guest added successfully.";
+            }
+        } catch (Exception e) {
+            return "An error occurred while adding the user: " + e.getMessage();
+        }
     }
 
-    return "User not found.";
-  }
+    public static String updateEmployeeOrGuest(String email, String newPassword, String newName, String newPhoneNumber) {
+        try {
+            for (Employee employee : assetPlus.getEmployees()) {
+                if (employee.getEmail().equals(email)) {
+                    employee.setPassword(newPassword);
+                    employee.setName(newName);
+                    employee.setPhoneNumber(newPhoneNumber);
+                    return "Employee updated successfully.";
+                }
+            }
+
+            for (Guest guest : assetPlus.getGuests()) {
+                if (guest.getEmail().equals(email)) {
+                    guest.setPassword(newPassword);
+                    guest.setName(newName);
+                    guest.setPhoneNumber(newPhoneNumber);
+                    return "Guest updated successfully.";
+                }
+            }
+
+            return "User not found.";
+        } catch (Exception e) {
+            return "An error occurred while updating the user: " + e.getMessage();
+        }
+    }
 }

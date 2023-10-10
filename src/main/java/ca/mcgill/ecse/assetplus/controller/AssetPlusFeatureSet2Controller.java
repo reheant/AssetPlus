2149ae85076663f61693c14df03d26c3776d145d
Tiffany <tiffanymiller0708@gmail.com>
2023-10-10 12:35:11 -0400

    imported asset plus application

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse.assetplus.controller;
 import ca.mcgill.ecse.assetplus.model.*;
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 
 public class AssetPlusFeatureSet2Controller {
     private static AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();

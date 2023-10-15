388dd257bcff02f45906b11a0c5ce338595778df
Luke <luke.freund@mail.mcgill.ca>
2023-10-15 04:03:10 -0400

    JavaDocs and extraction of validation methods

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet5Controller.java
@@ -3,61 +3,100 @@ package ca.mcgill.ecse.assetplus.controller;
 import ca.mcgill.ecse.assetplus.model.MaintenanceTicket;
 import ca.mcgill.ecse.assetplus.model.TicketImage;
 
+import java.security.InvalidParameterException;
 import java.util.List;
 
 public class AssetPlusFeatureSet5Controller {
 
-  public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
-    try {
-      // constraint - URL must not be empty or null (see AssetPlus constraints.txt)
-      if (imageURL == null || imageURL.isEmpty()) {
-        return "Image URL must not be empty or null";
-      }
-      MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
-      if (ticket == null) {
-        return String.format("String with ticketID %d not found", ticketID);
-      }
-      // constraint - two imageURLs of the same ticket cannot be the same (see AssetPlus constraints.txt)
-      List<TicketImage> images = ticket.getTicketImages();
-      if (images != null) {
-        for (TicketImage image : images) {
-          if (image.getImageURL().equals(imageURL)) {
-            return "Image not added to maintenance ticket - two imageURLs " +
-                    "of the same ticket cannot be the same: " + imageURL;
-          }
-        }
-      }
+    /**
+     * @param imageURL The URL of the image to be added to the maintenance ticket. Must not be empty or null, must
+     *                 start with either http:// or https://, two imageURLs of the same ticket cannot be the same.
+     * @param ticketID The unique identifier of the maintenance ticket.
+     * @return An empty string indicating success. An error message if failure.
+     * @author Luke Freund
+     */
+    public static String addImageToMaintenanceTicket(String imageURL, int ticketID) {
+        try {
+            MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
 
-      TicketImage image = new TicketImage(imageURL, ticket);
-      return String.format("Added image %s to ticket %d", image, ticketID);
-    } catch (Exception e) {
-      return "An unexpected error occurred while attempting to add an " +
-              "image to the maintenance ticket: " + e.getMessage();
-    }
-  }
+            String error = "";
+            error += assertValidImageURL(imageURL);
+            error += assertTicketExists(ticket, ticketID);
+            error += assertNoSameImageURL(imageURL, ticket);
 
-  public static void deleteImageFromMaintenanceTicket(String imageURL, int ticketID) {
-    try {
-      // constraint - URL must not be empty or null (see AssetPlus constraints.txt)
-      if (imageURL == null || imageURL.isEmpty()) {
-        return;
-      }
-      MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
-      if (ticket == null) {
-        return;
-      }
-      List<TicketImage> images = ticket.getTicketImages();
-      if (images != null) {
-        for (TicketImage image : images) {
-          if (image.getImageURL().equals(imageURL)) {
-            image.delete();
-            return;
-          }
+            if (!error.isEmpty()) {
+                return error.trim();
+            }
+
+            // Creates two-way association between image and maintenance ticket.
+            TicketImage image = new TicketImage(imageURL, ticket);
+
+            return "";
+        } catch (Exception e) {
+            return "An unexpected error occurred while attempting to add an " +
+                    "image to the maintenance ticket: " + e.getMessage();
         }
-      }
-    } catch (Exception e) {
-      // Log error
     }
-  }
+
+    /**
+     * @param imageURL The URL of the image to be removed from the maintenance ticket. Must not be empty or null, must
+     *                 start with either http:// or https://, two imageURLs of the same ticket cannot be the same.
+     * @param ticketID The unique identifier of the maintenance ticket.
+     * @author Luke Freund
+     */
+    public static void deleteImageFromMaintenanceTicket(String imageURL, int ticketID) {
+        MaintenanceTicket ticket = MaintenanceTicket.getWithId(ticketID);
+
+        String error = "";
+        error += assertTicketExists(ticket, ticketID);
+
+        if (!error.isEmpty()) {
+            throw new InvalidParameterException(
+                    "Ticket with provided ticket id does not exist in the system");
+        }
+
+        List<TicketImage> images = ticket.getTicketImages();
+        if (images != null) {
+            for (TicketImage image : images) {
+                if (image.getImageURL().equals(imageURL)) {
+                    image.delete();
+                    return;
+                }
+            }
+        }
+    }
+
+    private static String assertValidImageURL(String imageURL) {
+        if (imageURL == null || imageURL.isEmpty()) {
+            return "Image URL must not be empty or null";
+        }
+        if (!imageURL.startsWith("http://") && !imageURL.startsWith("http://")) {
+            return "Image URL must start with either http:// or https://";
+        }
+        return "";
+    }
+
+    private static String assertTicketExists(MaintenanceTicket ticket, int ticketID) {
+        if (ticket == null) {
+            return String.format("String with ticketID %d not found", ticketID);
+        }
+        return "";
+    }
+
+    private static String assertNoSameImageURL(String imageURL, MaintenanceTicket ticket) {
+        if (ticket == null) {
+            return "";
+        }
+        List<TicketImage> images = ticket.getTicketImages();
+        if (images != null) {
+            for (TicketImage image : images) {
+                if (image.getImageURL().equals(imageURL)) {
+                    return "Image not added to maintenance ticket - two imageURLs " +
+                            "of the same ticket cannot be the same: " + imageURL;
+                }
+            }
+        }
+        return "";
+    }
 
 }

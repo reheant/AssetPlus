3cf2a2b5763d007bcc0ac17172373068948d5a88
liamdichiro <liam.dichiro@mail.mcgill.ca>
2023-10-14 20:17:06 -0400

    Completed feature

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
@@ -1,40 +1,47 @@
 package ca.mcgill.ecse.assetplus.controller;
 
 import java.util.ArrayList;
+import java.sql.Date;
 import java.util.List;
-
+import java.lang.IllegalArgumentException;
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 import ca.mcgill.ecse.assetplus.model.*;
-import ca.mcgill.ecse.assetplus.controller.*;
 
 public class AssetPlusFeatureSet6Controller {
   private static AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
 
-  // Method 1
+  /**
+     * Deletes an Employee or Guest from the AssetPlus system
+     * @author Liam Di Chiro
+     * @param email the email of an Employee or Guest, must be of type String
+     */
+  
   public static void deleteEmployeeOrGuest(String email) {
     User user = User.getWithEmail(email);
 
     if (user == null) {
-      throw new NullPointerException("User not found.");
+      throw new IllegalArgumentException("User not found.");
     }
 
     user.delete();
   }
 
-  // Method 2
+    /**
+     * Returns a list of maintenance tickets from the AssetPlus system
+     * @author Liam Di Chiro
+     * @return A List of type TOMaintenanceTicket of all of the maintenance tickets 
+     */
   public static List<TOMaintenanceTicket> getTickets() {
     List<MaintenanceTicket> maintenanceTickets = assetPlus.getMaintenanceTickets();
     List<TOMaintenanceTicket> maintenanceTicketsTO = new ArrayList<>();
 
-    for (MaintenanceTicket maintenanceTicket : MaintenanceTickets){
+    for (MaintenanceTicket maintenanceTicket : maintenanceTickets){
       int id = maintenanceTicket.getId();
       Date raisedOnDate = maintenanceTicket.getRaisedOnDate();
       String description = maintenanceTicket.getDescription();
-
-      // INCOMPLETE 
       String raisedByEmail = maintenanceTicket.getTicketRaiser().getEmail(); 
       String assetName = maintenanceTicket.getAsset().getAssetType().getName();
-      int expectLifeSpanInDays = maintenanceTicket.getAsset().getExpectLifeSpanInDays(); //Getter?
+      int expectLifeSpan = maintenanceTicket.getAsset().getAssetType().getExpectedLifeSpan(); 
       Date purchaseDate = maintenanceTicket.getAsset().getPurchaseDate();
       int floorNumber = maintenanceTicket.getAsset().getFloorNumber();
       int roomNumber = maintenanceTicket.getAsset().getRoomNumber();
@@ -43,19 +50,17 @@ public class AssetPlusFeatureSet6Controller {
       List<String> imageURLs = new ArrayList<>();
 
       for (TicketImage ticketImage : ticketImages){
-        ImageURLs.add(ticketImage.getImageURL());
+        imageURLs.add(ticketImage.getImageURL());
       }
 
       List<MaintenanceNote> notes = maintenanceTicket.getTicketNotes();
-      List<TOMaintenanceNote> maintenanceNotesTO = new TOMaintenanceNote[notes.size()];
+      TOMaintenanceNote[] allNotes = new TOMaintenanceNote[notes.size()];
 
-      int index = 0;
-      for (MaintenanceNote maintenanceNote : notes) {
-        maintenanceNotesTO[index] = new TOMaintenanceNote(maintenanceNote.getDate(), maintenanceNote.getDescription(), maintenanceNote.getNoteTaker());
-        index++;
+      for (int i = 0; i < notes.size(); i++){
+        allNotes[i] = new TOMaintenanceNote(notes.get(i).getDate(), notes.get(i).getDescription(), notes.get(i).getNoteTaker().getEmail());
       }
 
-      TOMaintenanceTicket currTOMaintenanceTicket = new TOMaintenanceTicket(id, raisedOnDate, description, raisedByEmail, assetName, expectLifeSpanInDays, purchaseDate, floorNumber, roomNumber, imageURLs, maintenanceNotesTO);
+      TOMaintenanceTicket currTOMaintenanceTicket = new TOMaintenanceTicket(id, raisedOnDate, description, raisedByEmail, assetName, expectLifeSpan, purchaseDate, floorNumber, roomNumber, imageURLs, allNotes);
       maintenanceTicketsTO.add(currTOMaintenanceTicket);
     }
 

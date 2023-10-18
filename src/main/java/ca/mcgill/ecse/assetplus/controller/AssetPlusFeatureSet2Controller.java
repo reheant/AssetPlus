dc4de9175601d41ae841cbfd56ca2c0977b39314
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2023-10-18 16:06:40 -0400

    used java formatter for Tiff's controller

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet2Controller.java
@@ -1,11 +1,12 @@
 package ca.mcgill.ecse.assetplus.controller;
+
 import ca.mcgill.ecse.assetplus.model.*;
 import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
 
 public class AssetPlusFeatureSet2Controller {
-	
+
     private static AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
-    
+
     /**
      * @author Tiffany Miller
      * @param name The name of the asset type to create. Must not be empty or null.
@@ -16,7 +17,7 @@ public class AssetPlusFeatureSet2Controller {
         var error = "";
         error += assertNameValid(name);
         error += assertLifeSpanValid(expectedLifeSpanInDays);
-        
+
         if (!error.isEmpty()) {
             return error.trim();
         }
@@ -28,20 +29,23 @@ public class AssetPlusFeatureSet2Controller {
         }
         return "";
     }
-    
+
     /**
      * @author Tiffany Miller
-     * @param oldName The old name of the asset type to modify. Must not be empty or null. The old name must belong to an existing asset type.
+     * @param oldName The old name of the asset type to modify. Must not be empty or null. The old
+     *        name must belong to an existing asset type.
      * @param newName The new name of the asset type to modify. Must not be empty or null.
-     * @param newExpectedLifeSpanInDays The new expected life span in days of the asset type of which to modify. Must be > 0.
+     * @param newExpectedLifeSpanInDays The new expected life span in days of the asset type of
+     *        which to modify. Must be > 0.
      * @return An empty string indicating success. An error message if failure.
      */
-  public static String updateAssetType(String oldName, String newName, int newExpectedLifeSpanInDays) {
+    public static String updateAssetType(String oldName, String newName,
+            int newExpectedLifeSpanInDays) {
         var error = "";
         error += assertNameValid(newName);
         error += assertNameValid(oldName);
         error += assertLifeSpanValid(newExpectedLifeSpanInDays);
-        
+
         AssetType assetType = AssetType.getWithName(oldName);
         if (assetType == null) {
             error += "Old asset name is not valid. ";
@@ -57,45 +61,43 @@ public class AssetPlusFeatureSet2Controller {
             return e.getMessage();
         }
         return "";
-  }
-  
-  /**
-   * @author Tiffany Miller
-   * @param name The name of the asset type to delete. Must not be empty or null.
-   */
-  public static void deleteAssetType(String name) {
-      AssetType assetType = AssetType.getWithName(name);
-      if (assetType == null) {
-    	  throw new NullPointerException("No such asset type");
-      }
-      assetType.delete();    
-  }
-  
-  /**
-   * @author Tiffany Miller
-   * @param name The name of the asset type to validate. Must not be empty or null.
-   * @return An empty string indicating success. An error message if failure.
-   */
-  private static String assertNameValid(String name) {
-	  if (name == null || name.isEmpty()) {
-          return "A valid name must be inputted. ";
-      }
-	  else {
-		  return "";
-	  }
-  }
-  	
-  /**
-   * @author Tiffany Miller
-   * @param expectedLifeSpan The expected life span in days of the asset type. Must be > 0.
-   * @return An empty string indicating success. An error message if failure.
-   */
+    }
+
+    /**
+     * @author Tiffany Miller
+     * @param name The name of the asset type to delete. Must not be empty or null.
+     */
+    public static void deleteAssetType(String name) {
+        AssetType assetType = AssetType.getWithName(name);
+        if (assetType == null) {
+            throw new NullPointerException("No such asset type");
+        }
+        assetType.delete();
+    }
+
+    /**
+     * @author Tiffany Miller
+     * @param name The name of the asset type to validate. Must not be empty or null.
+     * @return An empty string indicating success. An error message if failure.
+     */
+    private static String assertNameValid(String name) {
+        if (name == null || name.isEmpty()) {
+            return "A valid name must be inputted. ";
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * @author Tiffany Miller
+     * @param expectedLifeSpan The expected life span in days of the asset type. Must be > 0.
+     * @return An empty string indicating success. An error message if failure.
+     */
     private static String assertLifeSpanValid(int expectedLifeSpan) {
         if (expectedLifeSpan <= 0) {
             return "Not a valid life span input. ";
-        }
-        else {
+        } else {
             return "";
         }
-    }  
+    }
 }

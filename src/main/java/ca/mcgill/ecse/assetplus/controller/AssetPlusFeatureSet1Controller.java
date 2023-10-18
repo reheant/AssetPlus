1902c2c5a9d62c4498713b9606d9d2c74e0e80fd
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2023-10-18 15:56:35 -0400

    formatted document

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet1Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet1Controller.java
@@ -8,9 +8,12 @@ public class AssetPlusFeatureSet1Controller {
 
     /**
      * Updates the password of the manager in the system.
+     * 
      * @author Nicolas Bolouri
-     * @param password The new password for the manager must be >=4 characters, contain !#$, an uppercase, and a lowercase letter.
-     * @return A string message indicating the failure of the operation or an empty string if the operation was successful.
+     * @param password The new password for the manager must be >=4 characters, contain !#$, an
+     *        uppercase, and a lowercase letter.
+     * @return A string message indicating the failure of the operation or an empty string if the
+     *         operation was successful.
      */
     public static String updateManager(String password) {
         var error = "";
@@ -30,17 +33,23 @@ public class AssetPlusFeatureSet1Controller {
 
     /**
      * Adds a new Employee or Guest to the AssetPlus system with the given details.
+     * 
      * @author Nicolas Bolouri
-     * @param email The email of the new user, following specific constraints for Employees and Guests.
+     * @param email The email of the new user, following specific constraints for Employees and
+     *        Guests.
      * @param password The password of the new user, cannot be null or empty.
      * @param name The name of the new user, cannot be null.
      * @param phoneNumber The phone number of the new user, cannot be null.
-     * @param isEmployee A boolean indicating whether the new user is an Employee (true) or Guest (false).
-     * @return A string message indicating the failure of the operation or an empty string if the operation was successful.
+     * @param isEmployee A boolean indicating whether the new user is an Employee (true) or Guest
+     *        (false).
+     * @return A string message indicating the failure of the operation or an empty string if the
+     *         operation was successful.
      */
-    public static String addEmployeeOrGuest(String email, String password, String name, String phoneNumber, boolean isEmployee) {
+    public static String addEmployeeOrGuest(String email, String password, String name,
+            String phoneNumber, boolean isEmployee) {
         var error = "";
-        error = validateEmployeeOrGuestEmail(email, isEmployee) + validatePassword(password) + validateName(name) + validatePhoneNumber(phoneNumber);
+        error = validateEmployeeOrGuestEmail(email, isEmployee) + validatePassword(password)
+                + validateName(name) + validatePhoneNumber(phoneNumber);
 
         if (!error.isEmpty()) {
             return error.trim();
@@ -60,16 +69,20 @@ public class AssetPlusFeatureSet1Controller {
 
     /**
      * Updates the details of an existing Employee or Guest in the AssetPlus system.
+     * 
      * @author Nicolas Bolouri
      * @param email The email of the user to be updated.
      * @param newPassword The new password to be set for the user, cannot be null or empty.
      * @param newName The new name to be set for the user, cannot be null.
      * @param newPhoneNumber The new phone number to be set for the user, cannot be null.
-     * @return A string message indicating the failure of the operation or an empty string if the operation was successful.
+     * @return A string message indicating the failure of the operation or an empty string if the
+     *         operation was successful.
      */
-    public static String updateEmployeeOrGuest(String email, String newPassword, String newName, String newPhoneNumber) {
+    public static String updateEmployeeOrGuest(String email, String newPassword, String newName,
+            String newPhoneNumber) {
         var error = "";
-        error = validateEmployeeOrGuestEmail(email, null) + validatePassword(newPassword) + validateName(newName) + validatePhoneNumber(newPhoneNumber);
+        error = validateEmployeeOrGuestEmail(email, null) + validatePassword(newPassword)
+                + validateName(newName) + validatePhoneNumber(newPhoneNumber);
 
         if (!error.isEmpty()) {
             return error.trim();
@@ -90,10 +103,12 @@ public class AssetPlusFeatureSet1Controller {
         return "";
     }
 
-     /**
+    /**
      * Checks if a manager is available in the AssetPlus system.
+     * 
      * @author Nicolas Bolouri
-     * @return An error message if the manager is not found, or an empty string if the manager is available.
+     * @return An error message if the manager is not found, or an empty string if the manager is
+     *         available.
      */
     private static String assertManagerAvailable() {
         return assetPlus.hasManager() ? "" : "Manager not found. ";
@@ -101,28 +116,32 @@ public class AssetPlusFeatureSet1Controller {
 
     /**
      * Validates the email of an Employee or Guest according to specific constraints.
+     * 
      * @author Nicolas Bolouri
      * @param email The email to be validated.
-     * @param isEmployee A Boolean indicating whether the email belongs to an Employee (true) or Guest (false); can be null.
-     * @return A specific error message if the email is invalid, or an empty string if the email is valid.
+     * @param isEmployee A Boolean indicating whether the email belongs to an Employee (true) or
+     *        Guest (false); can be null.
+     * @return A specific error message if the email is invalid, or an empty string if the email is
+     *         valid.
      */
     private static String validateEmployeeOrGuestEmail(String email, Boolean isEmployee) {
         if (email == null) {
             return "Email cannot be null. ";
         }
-        
+
         if (email.contains(" ")) {
             return "Email must not contain spaces. ";
         }
-        
+
         int atIndex = email.indexOf("@");
         int lastAtindex = email.lastIndexOf("@");
         int dotIndex = email.lastIndexOf(".");
-        
-        if (atIndex <= 0 || atIndex != lastAtindex || atIndex >= dotIndex - 1 || dotIndex >= email.length() - 1) {
+
+        if (atIndex <= 0 || atIndex != lastAtindex || atIndex >= dotIndex - 1
+                || dotIndex >= email.length() - 1) {
             return "Invalid email format. ";
         }
-    
+
         if (isEmployee != null) {
             if (isEmployee && !email.endsWith("@ap.com")) {
                 return "Employee email domain must be @ap.com. ";
@@ -131,18 +150,20 @@ public class AssetPlusFeatureSet1Controller {
             }
         }
 
-         if (email.equals("manager@ap.com")) {
+        if (email.equals("manager@ap.com")) {
             return "Employee or guest email cannot be equal to manager@ap.com. ";
         }
-        
+
         return "";
     }
 
     /**
      * Validates the password according to basic constraints.
+     * 
      * @author Nicolas Bolouri
      * @param password The password to be validated.
-     * @return A specific error message if the password is invalid, or an empty string if the password is valid.
+     * @return A specific error message if the password is invalid, or an empty string if the
+     *         password is valid.
      */
     private static String validatePassword(String password) {
         if (password == null) {
@@ -156,9 +177,11 @@ public class AssetPlusFeatureSet1Controller {
 
     /**
      * Validates the password of the manager according to specific constraints.
+     * 
      * @author Nicolas Bolouri
      * @param password The password to be validated.
-     * @return A specific error message if the password is invalid, or an empty string if the password is valid.
+     * @return A specific error message if the password is invalid, or an empty string if the
+     *         password is valid.
      */
     private static String validateManagerPassword(String password) {
         String error = validatePassword(password);
@@ -183,9 +206,11 @@ public class AssetPlusFeatureSet1Controller {
 
     /**
      * Validates the name according to basic constraints.
+     * 
      * @author Nicolas Bolouri
      * @param name The name to be validated.
-     * @return A specific error message if the name is invalid, or an empty string if the name is valid.
+     * @return A specific error message if the name is invalid, or an empty string if the name is
+     *         valid.
      */
     private static String validateName(String name) {
         return name != null ? "" : "Name cannot be null. ";
@@ -193,9 +218,11 @@ public class AssetPlusFeatureSet1Controller {
 
     /**
      * Validates the phone number according to basic constraints.
+     * 
      * @author Nicolas Bolouri
      * @param phoneNumber The phone number to be validated.
-     * @return A specific error message if the phone number is invalid, or an empty string if the phone number is valid.
+     * @return A specific error message if the phone number is invalid, or an empty string if the
+     *         phone number is valid.
      */
     private static String validatePhoneNumber(String phoneNumber) {
         return phoneNumber != null ? "" : "Phone number cannot be null. ";

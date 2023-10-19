bfeadfa836d654496d73dfc47ab94adaf4822ce6
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2023-10-19 11:38:28 -0400

    formatted

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
@@ -10,13 +10,13 @@ public class AssetPlusFeatureSet6Controller {
   private static AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
 
   /**
-    * Deletes an Employee or Guest from the AssetPlus system.
-    *
-    * @author Liam Di Chiro
-    * @param email The email of an Employee or Guest, must be of type String, and cannot be null.
-    */
+   * Deletes an Employee or Guest from the AssetPlus system.
+   *
+   * @author Liam Di Chiro
+   * @param email The email of an Employee or Guest, must be of type String, and cannot be null.
+   */
   public static void deleteEmployeeOrGuest(String email) {
-    if (email == null){
+    if (email == null) {
       throw new IllegalArgumentException("Email not found.");
     }
 
@@ -30,20 +30,17 @@ public class AssetPlusFeatureSet6Controller {
   }
 
   /**
-    * Returns a list of maintenance tickets from the AssetPlus system.
-    *
-    * @author Liam Di Chiro
-    *
-    * @return A List of type TOMaintenanceTicket of all of the maintenance tickets. 
-    */
+   * Returns a list of maintenance tickets from the AssetPlus system.
+   *
+   * @author Liam Di Chiro
+   *
+   * @return A List of type TOMaintenanceTicket of all of the maintenance tickets.
+   */
   public static List<TOMaintenanceTicket> getTickets() {
     List<MaintenanceTicket> maintenanceTickets = assetPlus.getMaintenanceTickets();
-
-    
-
     List<TOMaintenanceTicket> maintenanceTicketsTO = new ArrayList<>();
 
-    for (MaintenanceTicket maintenanceTicket : maintenanceTickets){
+    for (MaintenanceTicket maintenanceTicket : maintenanceTickets) {
       if (maintenanceTicket == null) {
         throw new IllegalArgumentException("Maintenance Ticket not found.");
       }
@@ -51,9 +48,9 @@ public class AssetPlusFeatureSet6Controller {
       int id = maintenanceTicket.getId();
       Date raisedOnDate = maintenanceTicket.getRaisedOnDate();
       String description = maintenanceTicket.getDescription();
-      String raisedByEmail = maintenanceTicket.getTicketRaiser().getEmail(); 
+      String raisedByEmail = maintenanceTicket.getTicketRaiser().getEmail();
       String assetName = maintenanceTicket.getAsset().getAssetType().getName();
-      int expectLifeSpan = maintenanceTicket.getAsset().getAssetType().getExpectedLifeSpan(); 
+      int expectLifeSpan = maintenanceTicket.getAsset().getAssetType().getExpectedLifeSpan();
       Date purchaseDate = maintenanceTicket.getAsset().getPurchaseDate();
       int floorNumber = maintenanceTicket.getAsset().getFloorNumber();
       int roomNumber = maintenanceTicket.getAsset().getRoomNumber();
@@ -65,49 +62,58 @@ public class AssetPlusFeatureSet6Controller {
       List<MaintenanceNote> notes = maintenanceTicket.getTicketNotes();
       TOMaintenanceNote[] allNotes = extractMaintenanceNotes(notes);
 
-      TOMaintenanceTicket currTOMaintenanceTicket = new TOMaintenanceTicket(id, raisedOnDate, description, raisedByEmail, assetName, expectLifeSpan, purchaseDate, floorNumber, roomNumber, imageURLs, allNotes);
+      TOMaintenanceTicket currTOMaintenanceTicket =
+          new TOMaintenanceTicket(id, raisedOnDate, description, raisedByEmail, assetName,
+              expectLifeSpan, purchaseDate, floorNumber, roomNumber, imageURLs, allNotes);
       maintenanceTicketsTO.add(currTOMaintenanceTicket);
     }
 
     return maintenanceTicketsTO;
-    
+
   }
+
   /**
-    * Extracts a list of ticket image URLs from a maintenance ticket images.
-    *
-    * @author Liam Di Chiro
-    * @param ticketImages the list of ticket images on a maintenance ticket, must be of type List<TicketImage>.
-    * @return A List of type String of all extracted image URLs of all of the images on a maintenance ticket. 
-    */
-  private static List<String> extractImageURLs(List<TicketImage> ticketImages){
+   * Extracts a list of ticket image URLs from a maintenance ticket images.
+   *
+   * @author Liam Di Chiro
+   * @param ticketImages the list of ticket images on a maintenance ticket, must be of type
+   *        List<TicketImage>.
+   * @return A List of type String of all extracted image URLs of all of the images on a maintenance
+   *         ticket.
+   */
+  private static List<String> extractImageURLs(List<TicketImage> ticketImages) {
     if (ticketImages == null) {
       throw new IllegalArgumentException("Ticket image not found.");
     }
-    
+
     List<String> imageURLs = new ArrayList<>();
-    for (TicketImage ticketImage : ticketImages){
+    for (TicketImage ticketImage : ticketImages) {
 
       imageURLs.add(ticketImage.getImageURL());
     }
     return imageURLs;
   }
+
   /**
-    * Extracts a list of maintenance notes from a maintenance ticket.
-    *
-    * @author Liam Di Chiro
-    * @param notes the list of maintenance notes to be added on to the maintenance ticket, must be of type List<MaintenanceNote>
-    * @return A List of type TOMaintenanceNote of all of the maintenance notes that have been converted to TO maintenance notes. 
-    */
-  private static TOMaintenanceNote[] extractMaintenanceNotes(List<MaintenanceNote> notes){
+   * Extracts a list of maintenance notes from a maintenance ticket.
+   *
+   * @author Liam Di Chiro
+   * @param notes the list of maintenance notes to be added on to the maintenance ticket, must be of
+   *        type List<MaintenanceNote>
+   * @return A List of type TOMaintenanceNote of all of the maintenance notes that have been
+   *         converted to TO maintenance notes.
+   */
+  private static TOMaintenanceNote[] extractMaintenanceNotes(List<MaintenanceNote> notes) {
     if (notes == null) {
       throw new IllegalArgumentException("Ticket image not found.");
     }
 
     TOMaintenanceNote[] allNotes = new TOMaintenanceNote[notes.size()];
-    for (int i = 0; i < notes.size(); i++){
-      allNotes[i] = new TOMaintenanceNote(notes.get(i).getDate(), notes.get(i).getDescription(), notes.get(i).getNoteTaker().getEmail());
+    for (int i = 0; i < notes.size(); i++) {
+      allNotes[i] = new TOMaintenanceNote(notes.get(i).getDate(), notes.get(i).getDescription(),
+          notes.get(i).getNoteTaker().getEmail());
     }
-    
+
     return allNotes;
   }
 }

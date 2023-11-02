addd5bc4795aee602304cb231d19bf02df219a56
Nicolas-Bolouri <nicolasbolouri@gmail.com>
2023-11-02 14:49:00 -0400

    reset to our controllers

--- src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
+++ src/main/java/ca/mcgill/ecse/assetplus/controller/AssetPlusFeatureSet6Controller.java
@@ -1,15 +1,37 @@
 package ca.mcgill.ecse.assetplus.controller;
 
+import java.util.ArrayList;
+import java.sql.Date;
 import java.util.List;
+import ca.mcgill.ecse.assetplus.application.AssetPlusApplication;
+import ca.mcgill.ecse.assetplus.model.*;
 
 public class AssetPlusFeatureSet6Controller {
+  private static AssetPlus assetPlus = AssetPlusApplication.getAssetPlus();
 
+  /**
+   * Deletes an Employee or Guest from the AssetPlus system.
+   *
+   * @author Liam Di Chiro
+   * @param email The email of an Employee or Guest, must be of type String, and cannot be null.
+   */
   public static void deleteEmployeeOrGuest(String email) {
-    // Remove this exception when you implement this method
-    throw new UnsupportedOperationException("Not Implemented!");
+    if (email == null) {
+      return;
+    }
+    User user = User.getWithEmail(email);
+    if (user == null || user instanceof Manager) {
+      return;
+    }
+    user.delete();
   }
 
-  // returns all tickets
+  /**
+   * Returns a list of maintenance tickets from the AssetPlus system.
+   *
+   * @author Liam Di Chiro
+   * @return A List of type TOMaintenanceTicket of all of the maintenance tickets.
+   */
   public static List<TOMaintenanceTicket> getTickets() {
     List<MaintenanceTicket> maintenanceTickets = assetPlus.getMaintenanceTickets();
     List<TOMaintenanceTicket> maintenanceTicketsTO = new ArrayList<>();
@@ -53,4 +75,48 @@ public class AssetPlusFeatureSet6Controller {
 
   }
 
+  /**
+   * Extracts a list of ticket image URLs from a maintenance ticket images.
+   *
+   * @author Liam Di Chiro
+   * @param ticketImages the list of ticket images on a maintenance ticket, must be of type
+   *        List<TicketImage>.
+   * @return A List of type String of all extracted image URLs of all of the images on a maintenance
+   *         ticket.
+   */
+  private static List<String> extractImageURLs(List<TicketImage> ticketImages) {
+    if (ticketImages == null) {
+      throw new IllegalArgumentException("Ticket image not found.");
+    }
+
+    List<String> imageURLs = new ArrayList<>();
+    for (TicketImage ticketImage : ticketImages) {
+
+      imageURLs.add(ticketImage.getImageURL());
+    }
+    return imageURLs;
+  }
+
+  /**
+   * Extracts a list of maintenance notes from a maintenance ticket.
+   *
+   * @author Liam Di Chiro
+   * @param notes the list of maintenance notes to be added on to the maintenance ticket, must be of
+   *        type List<MaintenanceNote>
+   * @return A List of type TOMaintenanceNote of all of the maintenance notes that have been
+   *         converted to TO maintenance notes.
+   */
+  private static TOMaintenanceNote[] extractMaintenanceNotes(List<MaintenanceNote> notes) {
+    if (notes == null) {
+      throw new IllegalArgumentException("Ticket image not found.");
+    }
+
+    TOMaintenanceNote[] allNotes = new TOMaintenanceNote[notes.size()];
+    for (int i = 0; i < notes.size(); i++) {
+      allNotes[i] = new TOMaintenanceNote(notes.get(i).getDate(), notes.get(i).getDescription(),
+          notes.get(i).getNoteTaker().getEmail());
+    }
+
+    return allNotes;
+  }
 }

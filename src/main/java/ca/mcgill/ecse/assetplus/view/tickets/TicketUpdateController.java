458bc3a42c75a367903e3bf86c683a9547ab2a0a
Julien Audet <jubiiz.audet@gmail.com>
2023-11-24 13:14:59 -0500

    visuals done for update ticket

--- src/main/java/ca/mcgill/ecse/assetplus/view/tickets/TicketUpdateController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/view/tickets/TicketUpdateController.java
@@ -3,13 +3,14 @@ package ca.mcgill.ecse.assetplus.view.tickets;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.AnchorPane;
+
 
 import java.io.IOException;
 import java.util.Objects;
 public class TicketUpdateController {
   @FXML
-  private VBox mainContentArea;
+  private AnchorPane mainContentArea;
 
   @FXML
   private void onHiClicked() {

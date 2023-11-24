0a6aeea6291c67476bec1bfc4d68a829757685f0
Julien Audet <jubiiz.audet@gmail.com>
2023-11-24 12:05:41 -0500

    created basic ticket update page

---
+++ src/main/java/ca/mcgill/ecse/assetplus/view/tickets/TicketUpdateController.java
@@ -0,0 +1,28 @@
+package ca.mcgill.ecse.assetplus.view.tickets;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.Objects;
+public class TicketUpdateController {
+  @FXML
+  private VBox mainContentArea;
+
+  @FXML
+  private void onHiClicked() {
+    System.out.println("omg hiiii!!");
+    //loadPage("manager.fxml");
+  }
+
+  private void loadPage(String fxmlFile) {
+      try {
+          Node page = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/ca/mcgill/ecse/assetplus/view/" + fxmlFile)));
+          mainContentArea.getChildren().setAll(page);
+      } catch (IOException e) {
+          e.printStackTrace();
+      }
+  }
+}

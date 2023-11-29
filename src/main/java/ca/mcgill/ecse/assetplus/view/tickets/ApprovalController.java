493d6c8107eb7bb45a9c98542c6bac9e511128b4
liamdichiro <liam.dichiro@mail.mcgill.ca>
2023-11-28 23:42:10 -0500

    beginning stages of approval

---
+++ src/main/java/ca/mcgill/ecse/assetplus/view/tickets/ApprovalController.java
@@ -0,0 +1,9 @@
+package ca.mcgill.ecse.assetplus.view.tickets;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class ApprovalController {
+    @FXML
+    private AnchorPane viewApprovalStatusContentArea;
+}

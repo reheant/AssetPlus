cd20bcbc268f39744a4ae2533f2d077381eb0987
Tiffany <tiffanymiller0708@gmail.com>
2023-11-29 20:42:41 -0500

    fixed error message

--- src/main/java/ca/mcgill/ecse/assetplus/view/AssetType/AddAssetTypeController.java
+++ src/main/java/ca/mcgill/ecse/assetplus/view/AssetType/AddAssetTypeController.java
@@ -33,12 +33,17 @@ public class AddAssetTypeController {
     String name = assetTypeName.getText().strip();
     String lifespan = assetTypeLifespan.getText().strip();
     String result = "";
-    try{
-      int lifeInt = Integer.parseInt(lifespan);
-      result += AssetPlusFeatureSet2Controller.addAssetType(name, lifeInt);
+    if (name.equals("") && lifespan.equals("")){
+      result += "Please enter name and lifespan to save";
     }
-    catch (Exception e){
-      result += "Please insert a valid integer";
+
+    else{
+      try {
+        int lifeInt = Integer.parseInt(lifespan);
+        result += AssetPlusFeatureSet2Controller.addAssetType(name, lifeInt);
+      } catch (Exception e) {
+        result += "Please insert a valid integer";
+      }
     }
 
     if (!result.equals("")) {
